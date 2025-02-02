7890ccf6bdcd0c8f73fa766460097004ddabbcd7
Tirth Patel <2024tirthpatel@gmail.com>
2025-02-02 15:19:26 +0530

    Add js file which is for functioning of book button

---
+++ content.js
@@ -0,0 +1,8 @@
+function login(){
+    location.href = "./login.html"
+}
+
+function book(){
+    alert("We simply charge 10 rupees for the platform; the rest amount is paid by you to the business owner based on the services you have used.")
+    location.href = "./payment.html"
+}

99c734a46996076e337613d204ff7d9cfed1a158
vikrant13572 <vikaassingh.70rain@gmail.com>
2025-02-05 21:40:10 +0530

    Update connection.php
    updated the database

--- connection.php
+++ connection.php
@@ -3,14 +3,10 @@
 $username = "root";
 $password = "";
 $server = 'localhost';
-$db = "practice";
+$db = "cc_project";
 
 $con = mysqli_connect($server,$username,$password,$db);
-   if($con){
-       echo "connection ok";
-   }else{
-       echo "connection nhi hua" ;
-   }
+   
 
 
 ?>

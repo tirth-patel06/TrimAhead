573e7a4ca6031989e2490fa4a7a1174c7400177c
Tirth Patel <2024tirthpatel@gmail.com>
2025-02-07 11:28:27 +0530

    fetch user info to connect with database

--- content.js
+++ content.js
@@ -4,5 +4,11 @@ function login(){
 
 function book(){
     alert("We simply charge 10 rupees for the platform; the rest amount is paid by you to the business owner based on the services you have used.")
-    location.href = "./payment.html"
+    const mobile = document.querySelector("#mobile").value;
+    const serial_no = document.querySelector("#serial_no").value;
+    const Queue = document.querySelector("#queue").value;
+    const Shop_Name = document.querySelector("#vname").value;
+    const Name = document.querySelector("#customer").value;
+
+    location.href = "./payment.php?mob="+mobile+"&vid="+serial_no+"&queue="+Queue+"&vname="+Shop_Name+"&cname="+Name
 }

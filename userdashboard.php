2ebf94eec2e324a64204015336e161003d540baf
Vikrant <vikaassingh.70rain@gmail.com>
2025-02-08 02:30:56 +0530

    Update and rename userdashboard.html to userdashboard.php

--- userdashboard.php
+++ userdashboard.php
@@ -1,3 +1,44 @@
+
+<?php
+session_start();
+?>
+
+<?php
+include 'connection.php';
+
+
+$mobile = $_POST['mobile'] ?? $_GET['mobile'] ?? '';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($mobile)) {
+    header("Location: userdashboard.php?mobile=" . urlencode($mobile));
+    exit();
+}
+
+
+
+
+
+
+
+    $selectquery = "SELECT * FROM `cc_project`.`customer` WHERE Phone_Number = '$mobile'";
+    $query = mysqli_query($con, $selectquery);
+
+    if ($query) {
+        $res = mysqli_fetch_array($query);
+
+        if ($res) {
+            $Name = $res['Name'];
+            $Email = $res['Email'];
+            $Phone_Number = $res['Phone_Number'];
+            $Address = $res['Address'];
+            $image = $res['user_image'];
+
+
+    }
+    }
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -26,52 +67,47 @@
         <aside class="sidebar">
             <h2>Dashboard</h2>
             <ul>
-                <li><a href="./alogin-index.html">Home</a></li>
-                <li><a href="./livebook.html">Live Booking</a></li>
-                <li><a href="./pBook.html">Previous Booking</a></li>
-                <li><a href="./index.html">Logout</a></li>
+                <li><a href="./alogin-index.php?mobile=<?php echo $Phone_Number;?>">Home</a></li>
+                <li><a href="./livebook.php?mobile=<?php echo $Phone_Number;?>">Live Booking</a></li>
+                <li><a href="./pBook.php?mobile=<?php echo $Phone_Number;?>">Previous Booking</a></li>
+                <li><a href="./index.php">Logout</a></li>
             </ul>
         </aside>
-        
+
         <main class="content">
             <div class="greet">
-                Welcome, 
+                Welcome,
                 <div class="greet-username">
-                    Tirth Patel
+                   <?php echo $Name;?>
                 </div>
             </div>
-            <div class="photo">
-                <img id="profile-img" src="./image/user-profile-photo.jpeg" alt="Profile Photo is not uploaded">
-                <input type="file" id="photo-input" name="profile-photo" accept="image/*" style="display: none;">
-                <div class="change">
-                    <button id="photo-edit" >Update</button>
-                    <span id="file-name" style="margin-left: 10px; font-size: 14px;"></span> <!-- To display file name -->
-                </div>
-            </div>
-            
+
+
             <div class="user-info">
-                <form action="" class="form-field" method="post">
+                <form action=""  class="form-field" method="post">
+                        <input type="hidden" name="mobile" value="<?php echo htmlspecialchars($mobile); ?>">
+
                     <div class="field">
                         <label for="name">Name</label>
-                        <input type="text" value="Tirth Patel" disabled>
+                        <input type="text" name = "Name" value="<?php echo $Name;?>" disabled>
                         <div class="change">
                             <button name="change" id="name" >Edit</button>
                         </div>
                     </div>
                     <div class="field">
                         <label for="mail">E-Mail</label>
-                        <input type="text" value="abc@demo.com" disabled>
+                        <input type="text" name = "Email " value="<?php echo $Email;?>" disabled>
                         <div class="change">
                             <button name="change" id="mail" >Edit</button>
                         </div>
                     </div>
                     <div class="field-phone">
                         <label for="phone">Phone Number</label>
-                        <input type="text" value="+91 0000000000" disabled>
+                        <input type="text" name = "Phone_Number" value="<?php echo $Phone_Number;?>" disabled>
                     </div>
                     <div class="field addr">
                         <label for="addr">Address</label>
-                        <textarea id="autoExpand" id="addr" disabled>1st Floor, 81/32, Lal Bahadur Shastri Marg, Near, Hotstuff Crossing, Above Blackberry Showroom, Civil Lines, Prayagraj, Uttar Pradesh</textarea>
+                        <textarea id="autoExpand" name = "Address" id="addr" disabled><?php echo $Address;?></textarea>
                         <script>
                         document.getElementById("autoExpand").addEventListener("input", function () {
                             this.style.height = "auto";
@@ -83,7 +119,7 @@
                             <button name="change" >Edit</button>
                         </div>
                     </div>
-                    <button type="submit" class="submit-button">Submit</button>
+                    <button type="submit" name = "Submit"  class="submit-button" onclick = submit() >Submit</button>
                 </form>
             </div>
 
@@ -96,3 +132,32 @@
     <script src="./userdashboard.js"></script>
 </body>
 </html>
+<?php
+include 'connection.php';   
+if(isset($_POST['Submit'])) {
+
+    $Phoneupdate = $_POST['mobile'];
+    $Name =  $_POST['Name'];
+    $Address = $_POST['Address'];
+    $Email =  $_POST['Email'];
+    $Phone_Number =  $_POST['Phone_Number'];
+
+    $updatequery = "UPDATE `cc_project`.`customer`
+                    SET Phone_Number = '$Phone_Number', Name='$Name', Address='$Address', Email='$Email'
+                    WHERE Phone_Number = '$Phoneupdate'";
+
+    $resupdate = mysqli_query($con, $updatequery);
+
+    if ($resupdate) {
+        echo "<script>alert('Update successful!');</script>";
+        header("Location: userdashboard.php?mobile=" . urlencode($Phone_Number));
+        exit();
+    } else {
+        echo "<script>alert('Update failed: " . mysqli_error($con) . "');</script>";
+    }
+}
+?>
+
+
+
+
